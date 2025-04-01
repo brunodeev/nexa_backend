5be1961f01f58814069e40b5301669f13e475b22
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-03-31 22:03:14 -0300

    feat: Supabase driver and connection stabilized

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "nexa/internal/service"
+import (
+	"fmt"
+	"log"
+	"nexa/internal/database"
+	"nexa/internal/service"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("ocorreu um erro ao carregar o .env: %v", err)
+	}
+	database.ConnectDB()
+	fmt.Println("Deu bom a conexão do DB")
 	service.HostServer()
 }
---
+++ internal/database/database.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func ConnectDB() {
+	DB_URI := os.Getenv("DB_URI")
+
+	conn, err := pgx.Connect(context.Background(), DB_URI)
+
+	if err != nil {
+		log.Fatalf("ocorreu um erro ao carregar o .env: %v", err)
+	}
+	defer conn.Close(context.Background())
+}
