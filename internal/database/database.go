5fc54573fd5243d4cf226aa1a006e27e2188171d
Bruno <bcgmeireles@gmail.com>
2025-04-01 22:43:01 -0300

    feat: implements gitignore and change start server name

--- internal/database/database.go
+++ internal/database/database.go
@@ -12,7 +12,6 @@ func ConnectDB() {
 	DB_URI := os.Getenv("DB_URI")
 
 	conn, err := pgx.Connect(context.Background(), DB_URI)
-
 	if err != nil {
 		log.Fatalf("ocorreu um erro ao carregar o .env: %v", err)
 	}
