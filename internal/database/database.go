53f0cd570f6ad4345effcb837fb3fb45686da28f
Bruno <bcgmeireles@gmail.com>
2025-04-04 16:56:30 -0300

    fix: solves db connection error

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"github.com/jackc/pgx/v5"
@@ -12,12 +13,20 @@ import (
 func ConnectDB() *pgx.Conn {
 	DB_URI := os.Getenv("DB_URI")
 
-	conn, err := pgx.Connect(context.Background(), DB_URI)
+	connConfig, err := pgx.ParseConfig(DB_URI)
+	if err != nil {
+		log.Fatalf("failed to parse DB URI: %v", err)
+	}
+
+	connConfig.DialFunc = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		return net.Dial("tcp4", addr)
+	}
+
+	conn, err := pgx.ConnectConfig(context.Background(), connConfig)
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
 
 	fmt.Println("Database connection successful.")
-
 	return conn
 }
